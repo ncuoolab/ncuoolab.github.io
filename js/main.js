9f7f33c1405ccb95d3bca1c16c23927a3d499a4f
Wei Cheng Pan <legnaleurc@gmail.com>
2013-07-20 19:25:04 +0800

    Use bootstrap template.

---
+++ js/main.js
@@ -0,0 +1,6 @@
+(function () {
+    $('.carousel').carousel();
+}());
+
+// ex: ts=4 sts=4 sw=4 et
+// sublime: tab_size 4; translate_tabs_to_spaces true; detect_indentation false; use_tab_stops true;
